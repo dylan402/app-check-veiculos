ea3d2baee2102d2c61da7b164966a01ebd98199f
Dylan Santiago <dylansantiagomelo@gmail.com>
2021-06-13 11:25:08 -0300

    Removendo comentário

--- app/src/main/java/br/checkveiculos/appcheckveiculos/activities/LoginActivity.java
+++ app/src/main/java/br/checkveiculos/appcheckveiculos/activities/LoginActivity.java
@@ -126,7 +126,7 @@ public class LoginActivity extends AppCompatActivity {
                     editor.commit();
 
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                     exibirLoading(false);
                 } else {
