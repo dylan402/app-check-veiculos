5bef3d1898143346f5a13805ff18bcf4c6be8b1b
Dylan Santiago <dylansantiagomelo@gmail.com>
2021-06-13 12:01:10 -0300

    Botão de sair

--- app/src/main/java/br/checkveiculos/appcheckveiculos/activities/MainActivity.java
+++ app/src/main/java/br/checkveiculos/appcheckveiculos/activities/MainActivity.java
@@ -1,8 +1,13 @@
 package br.checkveiculos.appcheckveiculos.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        this.sharedPreferences = this.getSharedPreferences("ClienteData", Context.MODE_PRIVATE);
 
         setSupportActionBar(binding.appBarMain.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
@@ -55,5 +63,22 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_sair:
+                SharedPreferences.Editor editor = this.sharedPreferences.edit();
+                editor.remove("id");
+                editor.commit();
+
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
